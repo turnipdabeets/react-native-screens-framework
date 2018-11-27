449e81b03750ea34ba23f96f32753cb8031bde83
Anna Garcia <anna.e.garcia@aexp.com>
2018-11-27 15:28:07 -0500

    Initial Commit

---
+++ RNScreens/RNScreens.h
@@ -0,0 +1,19 @@
+//
+//  RNScreens.h
+//  RNScreens
+//
+//  Created by Anna Elisabeth Garcia on 11/27/18.
+//  Copyright © 2018 Anna Elisabeth Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNScreens.
+FOUNDATION_EXPORT double RNScreensVersionNumber;
+
+//! Project version string for RNScreens.
+FOUNDATION_EXPORT const unsigned char RNScreensVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNScreens/PublicHeader.h>
+
+
